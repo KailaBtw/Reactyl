bbfd612381f8bac0b9a5345cd3c3631afb5dd8c4
Kaila Btw <ellielagrave@gmail.com>
2025-02-25 14:42:49 -0500

    * Initialize project * Set up npm, nodemon, etc. * Complete step 1 of tutorial (get a rotating cude working) * Use modules to get the Three.js packages, a more modern approach

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# MolMod---Molecular-Modeler
+# MolMod - Molecular Modeler
+
+## Configuration Directions
+
+* Keep track of configuration and setup directions
+
+###  Setup Instructions
+
+```{BASH}
+npm init
+npm install three
+nvm install 20
+nvm use 20
+
+# set up nodemon
+npm install nodemon -g
+nodemon mol_mod
+```
+
+### Run instructions
+
+* Use VScode "Run in liveserver"
+* Navigate to [Molecular Modeler localhost][mol_mod]
+
+
+## Todo List
+
+### Backend Changes
+
+* Move deployment into a docker container!
+
+### Frontend Changes
+
+* add Favicon
+    * https://stackoverflow.com/questions/1837261/how-to-animate-a-favicon
+    * https://favicon.io/
+
+
+
+[mol_mod]: http://localhost:5500/mol_mod/
---
+++ script.js
@@ -0,0 +1,33 @@
+// import * as THREE from 'three';
+
+// create canvas element
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+// Set up the renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Create the scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
+
+// Create a rudamentry shape (from three.js tutorial)
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+/**
+ * animate - called each time the scene is updated
+ */
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+// start animation loop
+animate();
