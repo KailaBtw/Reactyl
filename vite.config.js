90415151fa9a926d5b361b40105385ff86773aac
Kaila Btw <ellielagrave@gmail.com>
2025-05-07 18:16:20 -0400

    Fix GH-Pages loading issues
    Changes
    
    * Give vite a base path with my repo name  /MolMod-Molecular_Modeler/ if you examine my full url
    https://kailabtw.github.io/MolMod-Molecular_Modeler/
    For web deployments on gh pages it needs to know the FULL path, which it has no way of knowing locally.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // ... other configurations ...
-
+  base: '/MolMod-Molecular_Modeler/',
   // enable sourcemaps for useful debug messages in browser inspector
   build: {
     outDir: 'dist', // Output to the root of your project
